011f7ac70789239b74cb311ada2ab2633411297e
Weber Mori <webermori@gmail.com>
2017-10-10 20:01:47 -0300

    Datatype

--- js/main.js
+++ js/main.js
@@ -1,16 +1,37 @@
 $(function(){
     $('#AjaxRequest').submit(function(){
+        var form = $(this).serialize();
+        //var formArray = $(this).serializeArray();
+
+        // console.log(form);
+        // console.log(formArray);
+
         var request = $.ajax({
-            method:"POST",
+            method:"GET",
             url:"post.php",
-            data:{
-                name: $(':input[name=name]').val(),
-                email:$(':input[name=email]').val(),
-                tel:$(':input[name=tel]').val()
-            }
+            data: form,
+            dataType: "json"
+
+            // data:{
+            //     name: $(':input[name=name]').val(),
+            //     email:$(':input[name=email]').val(),
+            //     tel:$(':input[name=tel]').val()
+            // }
+        });
+
+        request.done(function(e){
+            console.log('done');
+            console.log(e.name);
+            console.log(e);
+        });
+
+        request.fail(function(e){
+            console.log('fail');
+            console.log(e);
         });
 
         request.always(function(e){
+            console.log('always');
             console.log(e);
         });
 
--- post.php
+++ post.php
@@ -1,9 +1,11 @@
 <?php 
 
 if ($_GET) {
-	var_dump($_GET);
+	//var_dump($_GET); exit;
+	//echo "<name>{$_GET['name']}</name>";
+	echo	json_encode($_GET); exit;
 }
 
 if($_POST) {
-	var_dump($_POST);
+	var_dump($_POST); exit;
 }
